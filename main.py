7e5410208a7fe942a5fc80a1e7444741a65714e5
andreea-munteanu <57788684+andreea-munteanu@users.noreply.github.com>
2021-01-10 01:50:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,6 @@ class Piece(object):
 
     # update board after dropping piece at position (row_count, col_count):
     def drop_piece(self, board, player_colour: (int, int, int), row_count: int, col_count: int):
-
         # 1 for player, 2 for CPU
         if player_colour == YELLOW:
             board[row_count][col_count] = 1
@@ -71,19 +70,16 @@ class Piece(object):
 # get next free row on column col
 def next_free_row_on_col(board, column):
     last = -1
-    for i in range(0, ROWS):
+    for i in range(ROWS):
         if board[i][column] == 0:
             last = i
-        else:
-            break
     return last  # -1 if column is full
 
 
 # remaining available moves (columns):
 def get_available_moves(board):
-    global COLS
     available_cols = []
-    for col_count in range(0, COLS):
+    for col_count in range(COLS):
         if next_free_row_on_col(board, col_count) != -1:
             available_cols.append(col_count)
     return available_cols
@@ -98,12 +94,106 @@ def first_available_column(board):
     return -1
 
 
-# pick optimal move for player of colour 'player_colour':
-def best_move(board, player_colour):  # 1 for human, 2 for CPU
+# compute score for player at a given time:
+def calculate_score(board, piece: int) -> int:
+    """
+    ________________ RULES__________________
+    player:
+
+    4-piece sequence --> player_score += 1000
+    3-piece sequence --> player_score += 500
+    2-piece sequence --> player_score += 250
+    1-piece sequence --> player_score += 50
+
+    opponent:
+    3-piece sequence --> player_score -= 200
+    __________________________
+    """
+    
+    def compute_score(seq_length: int):
+        total = 0
+        if seq_length == 1:
+            total += 50
+        elif seq_length == 2:
+            total += 250
+        elif seq_length == 3:
+            total += 500
+        elif seq_length == 4:
+            total += 1000
+        return total 
+        
+    # horizontally:
+    def horizontal_score() -> int:
+        total = 0
+        for row in range(ROWS):
+            seq_length = 1
+            for col in range(COLS-1):
+                if board[row][col] == board[row][col+1] == piece:
+                    seq_length += 1
+                else:
+                    total += compute_score(seq_length)
+                    seq_length = 1
+        return total
+
+    # vertically:
+    def vertical_score() -> int:
+        total = 0
+        for col in range(COLS):
+            seq_length = 1
+            for row in range(ROWS - 1):
+                if board[row][col] == board[row+1][col] == piece:
+                    seq_length += 1
+                else:
+                    total += compute_score(seq_length)
+                    seq_length = 1
+        return total
+
+    # diagonally:
+    def diagonal_score():
+        total = 0
+        def get_all_diagonals(): 
+            # main diagonals:
+            diags = [board[::-1, :].diagonal(i) for i in range(-board.shape[0] + 1, board.shape[1])]
+            # secondary diagonals:
+            diags.extend(board.diagonal(i) for i in range(board.shape[1] - 1, -board.shape[0], -1))
+            diags_list = []
+            for i in diags:
+                diags_list.append(list(i))
+            return diags_list  # as list
+
+            # all board diagonals:
+
+        diagonals = get_all_diagonals()
+
+        # parse all diagonals to check for 4 identical pieces:
+        for diag_i in diagonals:
+            seq_length = 1
+            for elem in range(len(diag_i) - 1):
+                if diag_i[elem] == diag_i[elem + 1] == piece:
+                    seq_length += 1
+                else:
+                    total += compute_score(seq_length)
+                    seq_length = 1
+        return total
+        
+    return horizontal_score() + vertical_score() + diagonal_score()
+
+
+# pick optimal move for player of colour 'piece':
+def best_move(board, piece: int):  # 1 for human, 2 for CPU
     available_moves = get_available_moves(board)  # track all possible moves
-    optimal_score = -100000
+    highest_score = -100000
     optimal_column = first_available_column(board)
-    # ###################### to be continued ################################
+    for col in available_moves:
+        row = next_free_row_on_col(board, col)
+        temp_board = board.copy()
+        piece = Piece(piece, temp_board)
+        # drop piece of colour 'piece'
+        piece.drop_piece(temp_board, piece, row, col)
+        score = 0 # score(board, player)
+        if score > highest_score:
+            highest_score, optimal_column = score, col
+    return optimal_column
 
 
 # variable for determining who won the game:
@@ -153,10 +243,10 @@ def check_win(board, player_colour: (int, int, int)) -> bool:
             diags = [board[::-1, :].diagonal(i) for i in range(-board.shape[0] + 1, board.shape[1])]
             # secondary diagonals:
             diags.extend(board.diagonal(i) for i in range(board.shape[1] - 1, -board.shape[0], -1))
-            diagonals = []
+            diags_list = []
             for i in diags:
-                diagonals.append(list(i))
-            return diagonals  # as list
+                diags_list.append(list(i))
+            return diags_list  # as list
 
         # all board diagonals:
         diagonals = get_all_diagonals()
@@ -196,6 +286,17 @@ def count_pieces(my_board, player_colour: (int, int, int)) -> int:
             if my_board[i][j] == val:
                 count += 1
     return count
+
+
+# printing board in terminal:
+def print_board(board):
+    for i in range(ROWS):
+        for j in range(COLS):
+            if j < COLS - 1:
+                print(int(board[i][j]), end='|')
+            else:
+                print(int(board[i][j]))
+    print('\n')
 
 
 # if opponent is computer, create buttons for levels (easy, medium, hard)
@@ -254,20 +355,10 @@ class GameBoard(object):
                 elif self.board[row_count][col_count] == 2:  # CPU player
                     circle_colour = RED
                 # initializing game_board.board piece with circle_colour
-                self.board_piece = Piece(circle_colour, self.board)
-                self.board_piece.draw_piece(circle_colour, row_count, col_count)
+                board_piece = Piece(circle_colour, self.board)
+                board_piece.draw_piece(circle_colour, row_count, col_count)
         # update display:
         pygame.display.update()
-
-    # printing board in terminal:
-    def printBoard(self):
-        for i in range(ROWS):
-            for j in range(COLS):
-                if j < COLS - 1:
-                    print(int(self.board[i][j]), end='|')
-                else:
-                    print(int(self.board[i][j]))
-        print('\n')
 
 
 game_board = GameBoard()
@@ -285,7 +376,7 @@ class Player(object):
     def inc_score(self):
         self.score += 1
 
-    def analyze_score(self):
+    def analyze_score(self, board, piece: int):
         pass
 
 
@@ -303,30 +394,10 @@ class AI(object):
         self.alpha = -1000000
         self.beta = 1000000
 
-    # calculate score:
-    def analyze_score(self, piece):
-        pass
-
-    # check locations for best move
-    # THIS ONE IS COPIED, MODIFY
-    # def best_move(self, piece) -> int:  # returns col
-    #     available_columns = get_available_moves()
-    #     best_score = 1000
-    #     best_column = random.choice(available_columns)
-    #     for col_count in available_columns:
-    #         row = next_free_row_on_col(col_count)
-    #         temp_board = self.board.copy()
-    #         drop_piece(row, col, piece)
-    #         score = analyze_score(temp_board, piece)
-    #         if score > best_score:
-    #             best_score = score
-    #             best_column = col
-    #     return best_column
-
-    # # make move (player of colour 'player' drops ball in column #col):
-    # def make_move(self, col, player):
-    #     row = next_free_row_on_col(col)
-    #     board[row][col] = player  # 1 for human, 2 for CPU
+    # make move (player of colour 'player' drops ball in column #col):
+    def make_move(self, board, col, player):
+        row = next_free_row_on_col(self.board, col)
+        self.board[row][col] = player  # 1 for human, 2 for CPU
 
     """ ______________________ EVAL FUNCTION ________________________"""
 
@@ -353,52 +424,6 @@ class AI(object):
                 pass
         if turn == 2:  # AI's turn
             pass
-
-    # def minimax(board, depth, alpha, beta, maximizingPlayer):
-    #     valid_locations = get_valid_locations(board)
-    #     is_terminal = is_terminal_node(board)
-    #     if depth == 0 or is_terminal:
-    #         if is_terminal:
-    #             if winning_move(board, AI_PIECE):
-    #                 return (None, 100000000000000)
-    #             elif winning_move(board, PLAYER_PIECE):
-    #                 return (None, -10000000000000)
-    #             else:  # Game is over, no more valid moves
-    #                 return (None, 0)
-    #         else:  # Depth is zero
-    #             return (None, score_position(board, AI_PIECE))
-    #     if maximizingPlayer:
-    #         value = -math.inf
-    #         column = random.choice(valid_locations)
-    #         for col in valid_locations:
-    #             row = get_next_open_row(board, col)
-    #             b_copy = board.copy()
-    #             drop_piece(b_copy, row, col, AI_PIECE)
-    #             new_score = minimax(b_copy, depth - 1, alpha, beta, False)[1]
-    #             if new_score > value:
-    #                 value = new_score
-    #                 column = col
-    #             alpha = max(alpha, value)
-    #             if alpha >= beta:
-    #                 break
-    #         return column, value
-    #
-    #     else:  # Minimizing player
-    #         value = math.inf
-    #         column = random.choice(valid_locations)
-    #         for col in valid_locations:
-    #             row = get_next_open_row(board, col)
-    #             b_copy = board.copy()
-    #             drop_piece(b_copy, row, col, PLAYER_PIECE)
-    #             new_score = minimax(b_copy, depth - 1, alpha, beta, True)[1]
-    #             if new_score < value:
-    #                 value = new_score
-    #                 column = col
-    #             beta = min(beta, value)
-    #             if alpha >= beta:
-    #                 break
-    #         return column, value
-
 
 def single_player():
     # difficulty: easy
@@ -465,7 +490,7 @@ def multiplayer():
                     piece.drop_piece(game_board.board, colour, drop_on_row, col)  # drop piece
                     FREE_CELLS = FREE_CELLS - 1
                     game_board.draw_board()  # update GUI board
-                    game_board.printBoard()  # print board
+                    print_board(game_board.board)  # print board
                     colour, turn = switch_player(colour, turn)  # switch players
                     # update score:
                     if turn == 2:
@@ -495,7 +520,7 @@ def multiplayer():
                     print('You won!\nScore:', human.score, '-', computer.score, '\nFree cells: ', FREE_CELLS)
                     color_fill = RED
                     text_box: str = "Player 2 wins! Congratulations!"
-                else:
+                else: # if winner == 3
                     print('You lost!\nTotal score:', human.score, '-', computer.score, '\nFree cells: ', FREE_CELLS)
                     color_fill = BLACK
                     text_box: str = "That was a close one! Tie."
@@ -506,7 +531,7 @@ def multiplayer():
                 winner_box = BoxMessage(H//3, W//3, W/2, 32, text_box)
                 winner_box.draw()
                 pygame.display.update()
-                time.sleep(2)
+                time.sleep(2) # sleep for two seconds after showing winner
                 # stop program from running:
                 running = False
 
